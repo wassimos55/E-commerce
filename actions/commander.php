ad8e185aeaae169b7db37f9087d4f7e5b952615f
wassimos55 <76432052+wassimos55@users.noreply.github.com>
2022-03-22 12:49:00 +0100

    3.0

--- actions/commander.php
+++ actions/commander.php
@@ -2,16 +2,16 @@
 session_start();
 //test user connectee
 
-if(!isset($_SESSION['nom'])){  //itha ken nom mouch mawjoud wela ay variable isset=mawjoud // ya3ni user non connecter
-  header('location:../connexion.php');
-  exit();
-}
+// if(!isset($_SESSION['nom'])){  //itha ken nom mouch mawjoud wela ay variable isset=mawjoud // ya3ni user non connecter
+//   header('location:../connexion.php');
+//   exit();
+// }
 
 // selectionner le produit avec leur id
 include "../inc/functions.php";
 
 // connexion bd
-
+ 
 $conn = connect();
 
 $visiteur = $_SESSION['id'];
@@ -19,16 +19,16 @@ $visiteur = $_SESSION['id'];
 
 
 
-//var_dump($_POST);
+// //var_dump($_POST);
 
 $id_produit = $_POST['produit'];
 $quantite = $_POST['quantite'];
 
 
 
-//requette
+// //requette
 
-$requette = "SELECT prix FROM produits WHERE id='$id_produit'"; 
+$requette = "SELECT prix, nom FROM produits WHERE id='$id_produit'"; 
 //execution requette 
 
 $resultat = $conn->query($requette);
@@ -39,23 +39,37 @@ $total = $quantite * $produit['prix'];
 
 $date = date("Y-m-d"); 
 
+if(!isset($_SESSION['panier'])){ //panier n'existe pas
+  $_SESSION['panier']= array($visiteur, 0 ,$date , array()); //tableau thanya reserve lel commande  // creation de panier
+}
+$_SESSION['panier'][1] += $total;
+$_SESSION['panier'][3][]= array($quantite, $total , $date , $date , $id_produit , $produit['nom']); //[] 5ater e type mta3a araay
 
-// creation de panier
-
-$requette_panier = "INSERT INTO paniers(visiteur,total,date_creation) VALUES('$visiteur','$total','$date')";
-//execution requette_pannier 
-
-$resultat = $conn->query($requette_panier);
-
-$panier_id = $conn->lastInsertId();
+/*echo "Panier <br/>";
+var_dump($_SESSION['panier']); 
+echo "Commande Panier <br/>";
+var_dump($_SESSION['panier'][3]); 
+exit;*/
 
 
-//Ajouter commande
-//requette
+// // creation de panier
 
-$requette = "INSERT INTO commandes(quantite,total,panier,date_creation,date_modification,produit) VALUES('$quantite','$total','$panier_id','$date','$date','$id_produit')"; 
-//execution requette 
+// $requette_panier = "INSERT INTO paniers(visiteur,total,date_creation) VALUES('$visiteur','$total','$date')";
+// //execution requette_pannier 
 
-$resultat = $conn->query($requette);
+// $resultat = $conn->query($requette_panier);
+
+// $panier_id = $conn->lastInsertId();
+
+
+// //Ajouter commande
+// //requette
+
+// $requette = "INSERT INTO commandes(quantite,total,panier,date_creation,date_modification,produit) VALUES('$quantite','$total','$panier_id','$date','$date','$id_produit')"; 
+// //execution requette 
+
+// $resultat = $conn->query($requette);
+
+header('location:../panier.php');
 
 ?>
---
+++ panier.php
@@ -0,0 +1,82 @@
+<?php 
+session_start();
+
+include "inc/functions.php";
+$categories = getAllCategory();
+
+if(!empty($_POST)){ //button search clicked
+    //echo "button search clicked";
+    //echo $_POST['search'];
+    $produits = seaechProduits($_POST['search']);
+}else{ 
+    $produits = getAllProducts();
+}
+
+if(isset($_SESSION['panier'])){
+    if(count($_SESSION['panier'])>0){
+        $commandes = $_SESSION['panier'][3];
+    }
+}
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>E-shop</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+<body>
+    <!--Start Navbar-->
+     <?php
+     include "inc/header.php";
+     ?>
+    <!--End Navbar-->
+    <!--Cards Start-->
+    <div class="row col-12 mt-4 p-5">
+        <h1>Panier Utilisateur</h1>
+        <!--Tabel Start-->
+        <table class="table">
+            <thead>
+                <tr>
+                <th scope="col">#</th>
+                <th scope="col">Produit</th>
+                <th scope="col">Quantite</th>
+                <th scope="col">Total</th>
+                <th scope="col">Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                   foreach($commandes as $index=>$commande){
+                       print ' 
+                        <tr>
+                            <th scope="row">'.($index+1).'</th>
+                            <td>'.$commande[5].'</td>
+                            <td>'.$commande[0].' Pieces</td>
+                            <td>'.$commande[1].' DT</td>
+                           <td><button class="btn btn-danger">Supprimer</button></td> 
+                        </tr>
+                       ';
+                   }
+                ?>
+            </tbody>
+            </table>
+        <!--Tabel End-->
+        <button class="btn btn-success" style="width:100px">Valider</button>
+
+    </div>
+    <!--Cards End-->
+    <!--Footer Start-->
+    <?php
+      include "inc/footer.php";
+    ?>
+    <!--Footer End-->
+    
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</html>
